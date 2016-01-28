3583c810e316c7eeab1f8fb7c0a7fc4e222461c7
antmachine <anthonysallows@gmail.com>
2016-01-27 16:35:50 -0800

    added noficiation modal, added route and controller for generating random strings, stubbed index view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,22 @@ class HomeController < ApplicationController
     @url = Url.new
   end
 
+  def crunch_it
+    url = Url.create(url_crunch_params)
+    letters_and_numbers = [*'0'..'9',*'A'..'Z',*'a'..'z']
+    random_letters_and_nubmers = Array.new(15){letters_and_numbers.sample}.join
+    url.crunched_url = random_letters_and_nubmers
+    url.save! ? flash[:notice] = "Url CRUNCHED!" : flash[:warning] = "Url not crunched, try again!"
+    redirect_to url_index_path
+  end
+
   def url_index
     @urls = Url.all
+  end
+
+
+  private
+  def url_crunch_params
+    params.require(:url).permit(:original_url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#url_crunch'
-  resources :urls, except: [:index]
+  resources :urls, except: [:index, :create]
   get '/all-urls' => "home#url_index", as: :url_index
+  post '/url-cruncher' => "home#crunch_it", as: :url_crunch
 end
